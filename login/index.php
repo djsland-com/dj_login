46a54b6efa42eb27bb009f5630a21e5d9eaabd0e
djsland.com <94417792+djsland-com@users.noreply.github.com>
2021-11-15 22:03:02 -0800

    Updated index.php to have labels

--- login/index.php
+++ login/index.php
@@ -8,10 +8,11 @@
     ?>
 
     <form method="post" action="login.php">
-        <input type="text" id="username" name="username" />
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username"  />
         <br>
+        <label for="password">Current Password</label>
         <input type="password" id="password" name="password" />
         <br>
-        <input type="submit" />
+        <input type="submit" value="Login" />
     </form>
-</center>
